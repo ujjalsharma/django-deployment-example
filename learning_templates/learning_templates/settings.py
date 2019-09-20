cedffa52f4740e373cdb4f7af9f2c7ee15339e1a
Ujjal Sharma <ujjalsharma81@gmail.com>
2019-09-21 03:01:25 +0530

    deleted templatetags

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -23,11 +23,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '+hd*4$tt7n$%$=%0(zs@lxhv3&h93ahmwi&n$_02#se6!5!jko'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    'ujjal10.pythonanywhere.com',
-]
+ALLOWED_HOSTS = ['ujjal10.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
